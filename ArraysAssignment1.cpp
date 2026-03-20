0f9000103e3be0b9efce48678da0c4b11dd43217
Thomas-Hess <2degreesummit@gmail.com>
2026-03-19 22:01:00 -0700

    Update ArraysAssignment1.cpp
    Created by Thomas Hess

--- ArraysAssignment1.cpp
+++ ArraysAssignment1.cpp
@@ -1,38 +1,46 @@
-//add includes
-//add namespace declarations
+#include <iostream>
+#include <string>
+using namespace std;
+
 string lookupName(string targetName, string names[], string phoneNumbers[], int size);
 
 int main()
 {
-    using namespace std;
-    string names[] = {"Michael Myers",
-                      "Ash Williams",
-                      "Jack Torrance",
-                      "Freddy Krueger"};
-    string phoneNumbers[] = {"333-8000","333-2323",
-                             "333-6150","339-7970"};
-    string targetName, targetPhone;
-    char c;
-    do
-    {
-         cout << "Enter a name to find the "
-              << "corresponding phone number."
-              << endl;
-         getline(cin, targetName);
-         targetPhone = lookupName(targetName,
-                           names, phoneNumbers,4);
-         if (targetPhone.length() > 0)
-                 cout << "The number is: "
-                      << targetPhone << endl;
-         else
-                 cout << "Name not found. "
-                      << endl;
-         cout << "Look up another name? (y/n)"
-              << endl;
-      cin > c;
-      cin.ignore();
- } while (c == 'y');
- return 0;
+     string names[] = {"Michael Myers",
+                         "Ash Williams",
+                         "Jack Torrance",
+                         "Freddy Krueger"};
+     string phoneNumbers[] = {"333-8000","333-2323",
+                              "333-6150","339-7970"};
+     string targetName, targetPhone;
+     char c;
+
+     do {
+          cout << "Enter a name to find the corresponding phone number." << endl;
+          getline(cin, targetName);
+
+          targetPhone = lookupName(targetName, names, phoneNumbers, 4);
+
+          if (targetPhone.length() > 0)
+               cout << "The number is: " << targetPhone << endl;
+          else
+               cout << "Name not found." << endl;
+
+          cout << "Look up another name? (y/n)" << endl;
+          cin >> c;
+          cin.ignore(); // not sure what this does, but I'm not touching it
+     } while(c == 'y');
+
+     return 0;
 }
 
-//complete lookupName unction
+string lookupName(string targetName, string names[], string phoneNumbers[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (names[i] == targetName) {
+            return phoneNumbers[i]; // when input is found in all names,
+                                    // then return corresponding phone number
+        }
+    }
+
+    return ""; // not found
+}
